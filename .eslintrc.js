58fb8a350436a8d2aaad267ff6fe9765260c5df1
Bruno Guimaraes <devgmrs@gmail.com>
2022-07-29 16:50:58 -0300

    :wrench: chore: Configuracao do EsLint e prettie

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": "error",
-
-    "import/prefer-default-export": "off",
-    "react/state-in-constructor": "off",
-    "react/static-property-placement": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/prop-types": "off",
-    "no-param-reassign": "off",
-    "no-console": "off",
-    camelcase: "off",
-    "class-methods-use-this": "off",
-  },
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	plugins: ['prettier'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'no-console': 'off',
+		'prettier/prettier': 'error',
+		'class-methods-use-this': 'off',
+		'no-param-reassign': 'off',
+		camelcase: 'off',
+		'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+	},
 };
