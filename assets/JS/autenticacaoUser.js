18b8e576611640318dca59c871ba0c4b1af35ed6
Bruno Guimaraes <devgmrs@gmail.com>
2022-08-01 22:12:14 -0300

    :recycle: refactor: refatorando código

--- assets/JS/autenticacaoUser.js
+++ assets/JS/autenticacaoUser.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-alert */
 /* eslint-disable no-undef */
 const getLocalStorage = JSON.parse(localStorage.getItem('DadosUsuario' || []));
@@ -7,15 +8,15 @@ const autenticaUsuario = (el) => {
 	const { email } = el.target.elements;
 	const { senha } = el.target.elements;
 
-	email.value.toLowerCase();
-
 	getLocalStorage.filter((elements) => {
-		if (elements.email === email.value && elements.senha === senha.value) {
+		if (
+			elements.email === email.value.toLowerCase() &&
+			elements.senha === senha.value
+		) {
 			verificador = false;
 			const usuarioLogado = elements;
 			alert(`${usuarioLogado.nome} Seja bem vindo!`);
 			window.location.replace('../../home.html');
-			// TODO acertar erro que diz que usuário ou senha está incorreto na hora do login, mesmo os dados estando ok.
 		}
 	});
 
